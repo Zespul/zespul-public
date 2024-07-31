e6d019163d13672e29467c52ffa590581cf609ff
DGrazz <mariogalo2008@gmail.com>
2024-07-31 10:36:10 +0200

    chore: Update Hero component to improve button alignment and spacing

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
                 <h1 className="text-4xl font-semibold">Encuentra el equipo que buscas, <span className="text-primary bg-secondary uppercase px-2">gratis</span></h1>
                 <p className="text-xs 2xl:text-sm">Busca equipo con el que propulsar tus ideas al éxito. Conéctate con expertos y creativos sin costes adicionales, publica tus proyectos y encuentra los colaboradores ideales.</p>
 
-                <div className="block space-x-8 text-xs 2xl:text-sm mt-4">
+                <div className="text-xs 2xl:text-sm flex gap-8 justify-start">
                     <Link href="/proyectos" className="bg-primary text-white font-bold px-6 py-4 rounded-md">Explorar proyectos</Link>
                     <Link href="/proyectos" className="border-2 border-black primary text-black font-bold px-6 py-4 rounded-md">Sube tu idea de proyecto</Link>
                 </div>
