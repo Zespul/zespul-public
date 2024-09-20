b33f0fbdc00fb832fc37393407e71ca36a053272
DGrazz <mariogalo2008@gmail.com>
2024-09-20 17:50:18 +0200

    Refactor metadata in layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,14 +5,40 @@ import Footer from "@/components/ui/Footer";
 
 const font = Inter({ subsets: ["latin"] });
 
+const sharedTitle = "Zespul";
+const sharedDescription = "Encuentra equipo gratis para tus proyectos.";
+const sharedImage = "https://zespul.vercel.app/zespul.png";
+
 export const metadata = {
-  title: "Zespul",
-  description: "Encuentra equipo gratis para tus proyectos.",
+  title: sharedTitle,
+  description: sharedDescription,
+
+  openGraph: {
+    title: sharedTitle,
+    description: sharedDescription,
+    url: "https://zespul.vercel.app/",
+    images: [
+      {
+        url: sharedImage,
+        width: 800,
+        height: 600,
+        alt: "Imagen de Zespul",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: sharedTitle,
+    description: sharedDescription,
+    images: [sharedImage],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <body className={`${font.className} min-h-screen bg-white text-black`}>
         <Navbar />
         {children}
